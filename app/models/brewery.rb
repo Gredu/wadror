569f32d6c16950b460e475d06dc7f72d75eb3db0
Greatman Lim <greatman.lim@mahti.org>
2015-01-18 13:29:16 +0200

    Tehtävä 2.3

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,3 +1,16 @@
 class Brewery < ActiveRecord::Base
+
     has_many :beers
+
+    def print_report
+        puts name
+        puts "Established at year #{year}"
+        puts "Numer of beers #{beers.count}"
+    end
+
+    def restart
+        self.year = 2015
+        puts "Changed year to #{year}"
+    end
+
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,9 @@
 class Rating < ActiveRecord::Base
+
     belongs_to :beer
+
+    def to_s
+        (Beer.find_by id: beer_id).name + " " + score.to_s
+    end
+
 end
