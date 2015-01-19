c4f43b88f0ec0a18204f11b6ff4d8c74e36bcc38
Greatman Lim <greatman.lim@mahti.org>
2015-01-19 19:16:36 +0200

    Tehtävä 2.4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,15 @@
 class Beer < ActiveRecord::Base
+
     belongs_to :brewery
     has_many :ratings
+
+    def average_rating
+        # sum = 0
+        # ratings.each do |rating|
+            # sum = sum + rating.score
+        # end
+        # average = sum / ratings.count
+        # return "Has " + ratings.count.to_s + " ratings, average " + average.to_s
+        ratings.average :score
+    end
 end
