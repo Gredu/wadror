d3d97f752add22557b8721a470c249afab6104fb
Greatman Lim <greatman.lim@mahti.org>
2015-01-24 16:07:05 +0200

    Tehtävä 2.14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,6 +1,7 @@
 class Brewery < ActiveRecord::Base
 
     has_many :beers, dependent: :destroy
+    has_many :ratings, through: :beers
 
     def print_report
         puts name
@@ -11,6 +12,10 @@ class Brewery < ActiveRecord::Base
     def restart
         self.year = 2015
         puts "Changed year to #{year}"
+    end
+
+    def average_rating
+        ratings.average :score
     end
 
 end
