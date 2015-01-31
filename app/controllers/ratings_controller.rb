c07059fe1e674c0769e9dbe8e6522d07c4021592
Greatman Lim <greatman.lim@mahti.org>
2015-01-31 14:16:51 +0200

    Tehtävä 3.4

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -10,16 +10,15 @@ class RatingsController < ApplicationController
 
     def create
         rating = Rating.create params.require(:rating).permit(:score, :beer_id)
-
+        current_user.ratings << rating
         # talletetaan tehdyn reittauksen sessioon
         session[:last_rating] = "#{rating.beer.name} #{rating.score} points"
-
         redirect_to ratings_path
     end
 
     def destroy
         rating = Rating.find(params[:id])
         rating.delete
-        redirect_to ratings_path
+        redirect_to :back
     end
 end
