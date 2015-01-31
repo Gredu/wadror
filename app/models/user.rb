cbc5b6a44934de882af93f8b3494e5a1413f818b
Greatman Lim <greatman.lim@mahti.org>
2015-01-31 17:38:54 +0200

    Tehtävä 3.10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ActiveRecord::Base
     include RatingAverage
 
+    # validates :username, length: { minimum: 3 }
+    has_many :ratings  # käyttäjällä on monta ratingia
+    has_many :beers, through: :ratings
+    has_secure_password
+
     validates :username, uniqueness: true,
         length: { minimum: 3, maximum: 15}
 
-    # validates :username, length: { minimum: 3 }
-    has_many :ratings  # käyttäjällä on monta ratingia
+    validates :password, length: { minimum: 4 },
+        format: { with: /(?=.*\d)(?=.*[A-Z])/, message: "password needs atleast one capital and one digit number" }
+
 end
