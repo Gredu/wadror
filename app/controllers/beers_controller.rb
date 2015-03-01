9e79009453b21c3bbf01e33412a37432fb498772
Greatman Lim <greatman.lim@mahti.org>
2015-03-01 15:59:29 +0200

    Beerlist with json and javascript

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -77,6 +77,9 @@ class BeersController < ApplicationController
     end
   end
 
+  def list
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_beer
